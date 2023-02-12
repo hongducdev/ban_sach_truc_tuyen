daa5ce4f5ae93abe9841e9505b6405625928da92
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2023-02-12 09:31:22 +0700

    Setup Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./modules/Layout/Layout";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route element={<Layout></Layout>}>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/modules/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Footer, Header } from '../../components'
+
+const Layout = () => {
+  return (
+    <div className="">
+      <Header />
+      <Outlet></Outlet>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout
